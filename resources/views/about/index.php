9d65256407d8d8faafa8dce134bb966352e79407
ACER <rrd636@gmail.com>
2019-11-05 18:13:39 +0700

    new template

--- resources/views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <h1 class="mt-3">About Me</h1>
-    <img src="<?= BASEURL; ?>/img/conglee.jpg" alt="Fikri Herlambang" width="200" class="rounded-circle shadow mb-3 mt-3">
-    <p>Halo Saya <?= $data['nama']; ?>, umur saya adalah <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?> </p>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,14 @@ Route::get('/coba', function () {
 });
 Route::get('/coba', function () {
     return view('/home/index');
+});Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 });
+
