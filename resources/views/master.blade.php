64a85b8066e1b5e69ccddb146879b8ed6ca61521
ACER <rrd636@gmail.com>
2019-11-13 12:59:37 +0700

    blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,16 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Start your development with a Design System for Bootstrap 4.">
   <meta name="author" content="Creative Tim">
-  <title>Welcome to Leaver</title>
+  <title>@yield('title')</title>
   <!-- Favicon -->
-  <link href="./assets/img/brand/favicon.png" rel="icon" type="image/png">
+  <link href="{{ asset('assets/img/brand/favicon.png') }}" rel="icon" type="image/png">
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
   <!-- Icons -->
-  <link href="./assets/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-  <link href="./assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
   <!-- Argon CSS -->
-  <link type="text/css" href="./assets/css/argon.css?v=1.1.0" rel="stylesheet">
+  <link type="text/css" href="{{ asset('assets/css/argon.css?v=1.1.0') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -22,7 +22,7 @@
     <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-transparent navbar-light headroom">
       <div class="container">
         <a class="navbar-brand mr-lg-5" href="{{ url('/') }}">
-          <img alt="image" src="./assets/img/brand/white.png">
+          <img src="{{ asset('assets/img/brand/white.png') }}" alt="brand">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -32,7 +32,7 @@
             <div class="row">
               <div class="col-6 collapse-brand">
                 <a href="./index.html">
-                  <img alt="image" src="./assets/img/brand/blue.png">
+                  <img src="{{ asset('assets/img/brand/blue.png') }}" alt="brand">
                 </a>
               </div>
               <div class="col-6 collapse-close">
@@ -124,56 +124,10 @@
       </div>
     </nav>
   </header>
-  <main>
-    <div class="position-relative">
-      <!-- Hero for FREE version -->
-      <section class="section section-lg section-hero section-shaped">
-        <!-- Background circles -->
-        <div class="shape shape-style-1 shape-primary">
-          <span class="span-150"></span>
-          <span class="span-50"></span>
-          <span class="span-50"></span>
-          <span class="span-75"></span>
-          <span class="span-100"></span>
-          <span class="span-75"></span>
-          <span class="span-50"></span>
-          <span class="span-100"></span>
-          <span class="span-50"></span>
-          <span class="span-100"></span>
-        </div>
-        <div class="container shape-container d-flex align-items-center py-lg">
-          <div class="col px-0">
-            <div class="row align-items-center justify-content-center">
-              <div class="col-lg-6 text-center">
-                <img alt="image" src="./assets/img/brand/white.png" style="width: 200px;" class="img-fluid">
-                <p class="lead text-white">Education revolution at it's fines.</p>
-                <div class="btn-wrapper mt-5">
-                  <a href="{{ url('login')}}" class="btn btn-lg btn-white btn-icon mb-3 mb-sm-0">
-                    <span class="btn-inner--icon"><i class="ni ni-cloud-download-95"></i></span>
-                    <span class="btn-inner--text">Login</span>
-                  </a>
-                  <a href="{{ url('register') }}" class="btn btn-lg btn-github btn-icon mb-3 mb-sm-0>
-                    <span class="btn-inner--icon"></span>
-                    <span class="btn-inner--text"><span class="text-warning">Sign</span> Up!</span>
-                  </a>
-                </div>
-                <div class="mt-5">
-                  <small class="text-white font-weight-bold mb-0 mr-2">*proudly coded by leaver team</small>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- SVG separator -->
-        <div class="separator separator-bottom separator-skew zindex-100">
-          <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <polygon class="fill-white" points="2560 0 2560 100 0 100"></polygon>
-          </svg>
-        </div>
-      </section>
-    </div>
+  @yield('body')
+  <footer class="footer">
     <div class="container">
-      <div class="row row-grid align-items-center my-md">
+      <div class="row row-grid align-items-center mb-5">
         <div class="col-lg-6">
           <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
           <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
@@ -220,16 +174,12 @@
     </div>
   </footer>
   <!-- Core -->
-  <script src="./assets/vendor/jquery/jquery.min.js"></script>
-  <script src="./assets/vendor/popper/popper.min.js"></script>
-  <script src="./assets/vendor/bootstrap/bootstrap.min.js"></script>
-  <script src="./assets/vendor/headroom/headroom.min.js"></script>
-  <!-- Optional JS -->
-  <script src="./assets/vendor/onscreen/onscreen.min.js"></script>
-  <script src="./assets/vendor/nouislider/js/nouislider.min.js"></script>
-  <script src="./assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+  <script src="{{asset("assets/vendor/jquery/jquery.min.js")}}"></script>
+  <script src="{{asset("assets/vendor/popper/popper.min.js")}}"></script>
+  <script src="{{asset("assets/vendor/bootstrap/bootstrap.min.js")}}"></script>
+  <script src="{{asset("assets/vendor/headroom/headroom.min.js")}}"></script>
   <!-- Argon JS -->
-  <script src="./assets/js/argon.js?v=1.1.0"></script>
+  <script src="{{asset("assets/js/argon.js?v=1.1.0")}}"></script>
 </body>
 
 </html>
