564e1f8560557f4b8aba4fb369981e5ebcbcbf3d
ACER <rrd636@gmail.com>
2019-11-27 13:44:59 +0700

    ha2

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,12 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
-=======
-use Faker\Factory as faker;
-use Illuminate\Support\Facades\DB;
-use App\User;
->>>>>>> 63af2219cef7cda1d38f3cb271147e8a8b0203a9
 
 class UserSeeder extends Seeder
 {
@@ -17,20 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         //
-=======
-        $faker = faker::create('id_ID');
-
-        for ($i=0; $i <= 10; $i++) { 
-            User::insert([
-                'id' => $i,
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => '12345',
-                'phone' => $faker->phoneNumber
-            ]);
-        }
->>>>>>> 63af2219cef7cda1d38f3cb271147e8a8b0203a9
     }
 }
